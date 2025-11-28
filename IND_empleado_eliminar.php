5b4f1b38964d70235653075739ccb88dd06ff265
Esteban <estebancadiz@gmail.com>
2025-11-28 10:37:19 +0000

    funcion empleado eliminar añadida

---
+++ IND_empleado_eliminar.php
@@ -0,0 +1,29 @@
+<?php
+include 'Conexion.php'; //$coneccion
+
+if($_POST["rut"]=="")//checkeo vacio
+{
+    echo '<h3>ERROR RUT VACIO</h3>';
+}else{
+
+    $rut=$_POST["rut"];
+    $existe_q="select rut from empleado where rut=$rut";
+    $existe_r=pg_query($coneccion,$existe_q);
+    if(pg_num_rows($existe_r)==0)//Checkeo si rut esta en la base de datos
+    {
+    echo '<h3>RUT NO EXISTE EN EL SISTEMA</h3>';
+    }else{
+    
+    $eliminar_q="DELETE FROM empleado WHERE rut=$rut;";
+    $eliminar_r=pg_query($coneccion,$eliminar_q);
+    if($eliminar_r)//si se elimina con exito
+    {
+	echo "<h3>Eliminado con exito de la base de datos</h3>";
+    }
+	
+    }
+    
+}
+
+
+?>
