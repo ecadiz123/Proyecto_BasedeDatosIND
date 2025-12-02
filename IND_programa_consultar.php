35974bb2d6880da607297cf6b8ab7fa33d138a90
Esteban <estebancadiz@gmail.com>
2025-12-02 21:21:45 +0000

    empleados añadidos a consultar programa

--- IND_programa_consultar.php
+++ IND_programa_consultar.php
@@ -20,7 +20,7 @@ if($_POST["nombre"]!='')//si no quedaron vacios
     
     echo "<b><h2>Resultados</h2> </b>";
 
-	while($resultado_busqueda=pg_fetch_array($nombre_r))
+	while($resultado_busqueda=pg_fetch_array($nombre_r))//impresion de programa
 	{
 	$id=$resultado_busqueda["id"];
 	$nombre=$resultado_busqueda["nombre"];
@@ -33,7 +33,45 @@ if($_POST["nombre"]!='')//si no quedaron vacios
 	    echo "<b><h4>Descripcion</h4></b>";
 	    echo $descripcion;
 	    echo "<hr>";
+	    echo "<hr>";
+	echo "<details>";
+	    echo "<summary>Empleados Asociados</summary>";//Impresion de los empleados
+	echo '<table>';
+	echo '<tr>';
+	echo '<td>Nombre</td>';
+	echo '<td>Area</td>';
+	echo '<td>Region</td>';
+	echo '</tr>';
+	
+	//se buscan empleados asociados al programa junto con su area y region respectivas
+	$empleados_q="	select e.nombre,a.nombre as area,r.nombre as region
+			from empleado e,supervisa s , programa_deporte pd,direccion_regional dr,region r ,area a   
+			where e.rut=s.rut_empleado 
+			and pd.id=s.id_programas_deporte 
+			and dr.id =e.direccion_regional 
+			and r.id =dr.region 
+			and e.id_area =a.id 
+			and pd.id =$id";
+
+	$empleados_r=pg_query($coneccion,$empleados_q);
+	while($empleado_datos=pg_fetch_array($empleados_r))
+	{	
+	$n_empleado=$empleado_datos["nombre"];
+	$area_empleado=$empleado_datos["area"];
+	$region_empleado=$empleado_datos["region"];
+	echo '<tr>';
+	echo '<td>'.$n_empleado.'</td>';
+	echo '<td>'.$area_empleado.'</td>';
+	echo '<td>'.$region_empleado.'</td>';
+	echo '</tr>';
+	}
+
+	echo '</table>';
 	echo "</details>";
+	echo "</details>";
+	    echo "<hr>";
+	echo '<br>';
+	echo '<br>';
 	 }
 	echo '<a href="IND_programa_consultar.html">Volver</a>';
     }
